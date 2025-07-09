8e33af97718557dc999badb9ddee919ad4862b40
Maaz-Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2025-07-09 21:12:18 +0500

    learn next auth

---
+++ types.d.ts
@@ -0,0 +1,14 @@
+import {Connection} from 'mongoose';
+
+
+
+declare global {
+
+    var mongoose: {
+        conn: Connection | null;        
+        promise: Promise<Connection> | null;
+    };
+}
+
+
+export {}
